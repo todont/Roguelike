a190a7694f205bb89667dda0af591aaa00ccbef8
Michael Polyntsov <Nikos_9148@mail.ru>
2019-10-13 23:54:43 +0300

    Added GameEngine.cs

---
+++ GameEngine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike
+{
+    class GameEngine
+    {
+        public void StartGame()
+        {
+            Location location = new Location();
+            location.Draw();
+	        Hero hero = new Hero();
+            while (true)
+            {
+                hero.Move(); 
+                location.Redraw();
+            }
+        }
+    }
+}
