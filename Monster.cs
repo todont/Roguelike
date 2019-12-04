da8f73f855e28100da93a8ef44f882a71e174eb5
Ivan Morozko <vanya.morozko@mail.ru>
2019-12-04 17:02:38 +0300

    Add simple machine-learning based AI to monsters

--- Monster.cs
+++ Monster.cs
@@ -18,19 +18,131 @@ namespace Roguelike
             Name = name;
             Symbol = symbol;
             IsMoved = false;
+            ChanceToGoUp = 0.25;
+            ChanceToGoDown = 0.25;
+            ChanceToGoRight = 0.25;
+            ChanceToGoLeft = 0.25;
+            StandardDecreasingChance = 0.025;
+            PrevDistance = 0;
         }
         //Tile MonsterTile (color and shape of 
         // symbol that represents monster)
+        private double ChanceToGoUp;
+        private double ChanceToGoDown;
+        private double ChanceToGoRight;
+        private double ChanceToGoLeft;
+        private double StandardDecreasingChance;
+        private double PrevDistance;
         public enum GameAction 
         {
             Attack
         }
         public GameAction CurrentGameAction { get; set; }
+        private void DecreaseChances()
+        {
+            ChanceToGoUp -= StandardDecreasingChance;
+            ChanceToGoDown -= StandardDecreasingChance;
+            ChanceToGoRight -= StandardDecreasingChance;
+            ChanceToGoLeft -= StandardDecreasingChance;
+        }
+        private void IncreaseChances()
+        {
+            ChanceToGoUp += StandardDecreasingChance;
+            ChanceToGoDown += StandardDecreasingChance;
+            ChanceToGoRight += StandardDecreasingChance;
+            ChanceToGoLeft += StandardDecreasingChance;
+        }
+        private void SetChances(double distance, BaseCharacter.MoveAction direction)
+        {
+            if (distance < PrevDistance)
+            {
+                DecreaseChances();
+                switch (direction)
+                {
+                    case MoveAction.Up:
+                        ChanceToGoUp += StandardDecreasingChance * 4;
+                        break;
+                    case MoveAction.Down:
+                        ChanceToGoDown += StandardDecreasingChance * 4;
+                        break;
+                    case MoveAction.Right:
+                        ChanceToGoRight += StandardDecreasingChance * 4;
+                        break;
+                    case MoveAction.Left:
+                        ChanceToGoLeft += StandardDecreasingChance * 4;
+                        break;
+                }
+            }
+            else
+            {
+                IncreaseChances();
+                switch (direction)
+                {
+                    case MoveAction.Up:
+                        ChanceToGoUp -= StandardDecreasingChance * 4;
+                        break;
+                    case MoveAction.Down:
+                        ChanceToGoDown -= StandardDecreasingChance * 4;
+                        break;
+                    case MoveAction.Right:
+                        ChanceToGoRight -= StandardDecreasingChance * 4;
+                        break;
+                    case MoveAction.Left:
+                        ChanceToGoLeft -= StandardDecreasingChance * 4;
+                        break;
+                }
+            }
+        }
+        private void SetStandardChances()
+        {
+            ChanceToGoUp = 0.25;
+            ChanceToGoDown = 0.25;
+            ChanceToGoRight = 0.25;
+            ChanceToGoLeft = 0.25;
+        }
+        private void SetMoveAction()
+        {
+            double randnumber = Program.GameEngine.GameRandom.NextDouble();
+            if (randnumber < ChanceToGoUp)
+            {
+                CurrentMoveAction = BaseCharacter.MoveAction.Up;
+                return;
+            }
+            if (randnumber < ChanceToGoUp + ChanceToGoDown)
+            {
+                CurrentMoveAction = BaseCharacter.MoveAction.Down;
+                return;
+            }
+            if (randnumber < ChanceToGoUp + ChanceToGoDown + ChanceToGoRight)
+            {
+                CurrentMoveAction = BaseCharacter.MoveAction.Right;
+                return;
+            }
+            CurrentMoveAction = BaseCharacter.MoveAction.Left;
+            Console.WriteLine(CurrentMoveAction);
+        }
+
+        public void MoveTo(BaseCharacter enemy)
+        {
+            double distance;
+
+            if (Coords.GetDistance(enemy.Coords) > RangeOfVision)
+            {
+                IsMoved = false;
+                return;
+            }
+            SetMoveAction();
+            Move();
+            distance = Coords.GetDistance(enemy.Coords);
+            SetChances(distance, CurrentMoveAction);
+            if (IsMoved) PrevDistance = distance;
+        }
         protected override bool HandleCollisions(char mapSymbol, char entitySymbol)
         {
             switch (mapSymbol)
             {
                 case '▒':
+                    SetStandardChances();
                     return false;
                 default:
                     return true;
